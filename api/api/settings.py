0933e9123d3a6a0ee6a97cd86377091d8d8766f1
Patrik <p.patrik08@outlook.com>
2018-11-05 10:58:37 +0100

    update settings

--- api/api/settings.py
+++ api/api/settings.py
@@ -79,9 +79,13 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bdtnswci',
+        'USER': 'bdtnswci',
+        'PASSWORD': '9WQW2RU6P5lSmLU1D4ddcHMMUzg2J6gH',
+        'HOST': 'dumbo.db.elephantsql.com',
+        'PORT': '',
+    }
 }
 
 # Password validation
